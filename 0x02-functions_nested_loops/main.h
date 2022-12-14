7e8da4f844101ec78afd1c313c9bc9e0fe0139a9
patrickolumba <patrickdalington11@gmail.com>
2022-12-14 04:39:26 -0800

    Hot fix

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,6 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-int times_table(void);
+void times_table(void);
 void print_to_98(int n);
 void print_times_table(int n);
