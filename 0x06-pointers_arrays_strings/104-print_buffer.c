bee052a1d75bab664fa3fe4b9dd0ace51abd594b
patrickolumba <patrickdalington11@gmail.com>
2022-12-22 06:03:42 -0800

    Adding last task

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -20,3 +20,34 @@ void print_buffer(char *b, int size)
 		for (x = 0; x < size; x += 10)
 		{
 			printf("%.8:", x);
+			for (y = x; y < x + 10; y++)
+			{
+				if (y % 2 == 0)
+					printf(" ");
+				if (y < size)
+					printf("%.2x", *(b + y));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (z = y; z < y + 10; z++)
+			{
+				if (z >= size)
+					break;
+				if (*(b + z) < 32 || *(b + z) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + z));
+			}
+			printf("\n");
+		}
+	}
+}
+
+
+
+
+
+
+
+
