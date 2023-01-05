169d5a1286f2e84c0b77284fbb6b2614fff5408e
patrickolumba <patrickdalington11@gmail.com>
2023-01-04 20:29:19 -0800

    Recursion 0x08

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,5 @@
-#include <unistd.h>
-
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -12,3 +12,4 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s2, char *s2);
 
+#endif
