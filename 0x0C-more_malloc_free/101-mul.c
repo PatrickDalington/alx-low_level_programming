a67710d413b4212810025e40bfad270b315b4eb7
patrickolumba <patrickdalington4@gmail.com>
2023-01-14 18:00:54 +0100

    More Malloc Free in C programming

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -92,12 +92,16 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 		printf("Error\n"), exit(98);
+
 	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
 	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+
 	if (nout == NULL)
 		printf("Error\n"), exit(98);
+
 	nout = _initialize_array(nout, lnout);
 	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+
 	for (; k >= 0; k--, i--)
 	{
 		if (i < 0)
@@ -109,8 +113,10 @@ int main(int argc, char *argv[])
 					nout[k - 1] = (add / 10) + '0';
 				nout[k] = (add % 10) + '0';
 			}
+
 			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
 		}
+
 		if (j < 0)
 		{
 			if (nout[0] != '0')
@@ -119,6 +125,7 @@ int main(int argc, char *argv[])
 			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
 			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 		}
+
 		if (j >= 0)
 		{
 			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
